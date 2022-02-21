f5bfd3cb15fa53851435fc29954adb0be58bd7ae
peterszabo79 <peterszabo79@gmail.com>
2022-02-21 21:52:53 +0000

    try to put it together

--- assets/js/script.js
+++ assets/js/script.js
@@ -78,10 +78,10 @@ function showScores() {
 // create questions here
 var questions = [
     new Question("Hyper Text Markup Language Stand For?", ["JavaScript", "XHTML","CSS"], "CSS"),
-    new Question("Which language is used for styling web pages?", ["HTML", "JQuery", "CSS", "XML"], "CSS"),
-    new Question("Which is not a JavaScript Framework?", ["Python Script", "JQuery","Django", "NodeJS"], "Django"),
-    new Question("Which is used for Connect To Database?", ["PHP", "HTML", "JS", "All"], "PHP"),
-    new Question("Webdevtrick.com is about..", ["Web Design", "Graphic Design", "SEO & Development", "All"], "All")
+    new Question("Which language is used for styling web pages?", ["HTML", "JQuery", "CSS"], "CSS"),
+    new Question("Which is not a JavaScript Framework?", ["Python Script", "JQuery","Django"], "Django"),
+    new Question("Which is used for Connect To Database?", ["PHP", "HTML", "All"], "PHP"),
+    new Question("Webdevtrick.com is about..", ["Web Design", "Graphic Design", "All"], "All")
 ];
  
 // create quiz
